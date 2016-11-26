3d075d8460c41c470e03d5491191a4b48e5c1ef8
theDataSmith <theDataSmith1@gmail.com>
2016-11-26 00:53:11 -0800

    LED Header Comments

--- LED.h
+++ LED.h
@@ -1,34 +1,74 @@
 #pragma once
 
-class LED
+namespace EuclidRobot
 {
-public:
-	LED(int pinLED, int pinR, int pinG, int pinB)
-		: pinLED(pinLED), pinR(pinR), pinG(pinG), pinB(pinB) {}
-	
-	void setOn(bool on = true);
-	void setOff(bool off = true);
+	class LED
+	{
+	public:
+		/*
+		Sets the mode of the pins.
+		*/
+		LED(int pinLED, int pinR, int pinG, int pinB);
 
-	void setR(bool on);
-	void setR(int value);
-	void setR(float value);
+		/*
+		Turns the LED on the Teensy board on or off.
+		*/
+		void setOn(bool on = true);
+		void setOff(bool off = true);
 
-	void setG(bool on);
-	void setG(int value);
-	void setG(float value);
+		/*
+		Sets the brightness of the red part of the RGB LED.
+		A bool turns it on or off, an int sets the brightness between 0-255,
+		and a float sets the brightness between 0-1.
+		*/
+		void setR(bool on);
+		void setR(int value);
+		void setR(float value);
 
-	void setB(bool on);
-	void setB(int value);
-	void setB(float value);
+		/*
+		Sets the brightness of the green part of the RGB LED.
+		A bool turns it on or off, an int sets the brightness between 0-255,
+		and a float sets the brightness between 0-1.
+		*/
+		void setG(bool on);
+		void setG(int value);
+		void setG(float value);
 
-	void setRGB(int r, int g, int b);
-	void setRGB(float r, float g, float b);
+		/*
+		Sets the brightness of the blue part of the RGB LED.
+		A bool turns it on or off, an int sets the brightness between 0-255,
+		and a float sets the brightness between 0-1.
+		*/
+		void setB(bool on);
+		void setB(int value);
+		void setB(float value);
 
-	void setHSV(float h, float s, float v);
+		/*
+		Sets the brightnesses of all three parts of the RGB LED at once.
+		An int should be between 0-255, and a float should be between 0-1.
+		*/
+		void setRGB(int r, int g, int b);
+		void setRGB(float r, float g, float b);
 
-private:
-	int pinLED;
-	int pinR;
-	int pinG;
-	int pinB;
-};
+		/*
+		Sets the color of the RGB LED by hue, saturation, and value.
+		Values between 0-1 should be used.
+		*/
+		void setHSV(float h, float s, float v);
+
+	private:
+		/*
+		The pin number of the LED on the Teensy board.
+		*/
+		int pinLED;
+
+		/*
+		The pin numbers of the three parts of the RGB LED.
+		*/
+		int pinR;
+		int pinG;
+		int pinB;
+
+		void setLed(int pin, int value);
+	};
+}
