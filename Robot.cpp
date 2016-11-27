b4ab2ab2188dd757d5608ea2279b80cc78e11d87
theDataSmith <theDataSmith1@gmail.com>
2016-11-26 18:46:51 -0800

    Added Comments to Robot

--- Robot.cpp
+++ Robot.cpp
@@ -1,3 +1,20 @@
+/*
+--FILE--
+Robot.cpp
+
+--AUTHOR--
+Name:		Josh Alan
+GitHub:		theDataSmith
+E-mail:		thedatasmith1@gmail.com
+
+--PROJECT--
+Euclid, the line-following robot.
+GitHub:		github.com/theDataSmith/euclid
+
+--CREATION DATE--
+11 / 26 / 2016
+*/
+
 #include "Robot.h"
 
 namespace EuclidRobot
@@ -8,26 +25,31 @@ namespace EuclidRobot
 		, leftMotor(PIN_MOTOR_LEFT_FWD, PIN_MOTOR_LEFT_BWD)
 		, rightMotor(PIN_MOTOR_RIGHT_FWD, PIN_MOTOR_RIGHT_BWD)
 	{
+		//Turn on the LED on the Teensy board.
 		led.setOn();
 	}
 
 	void Robot::countDown()
 	{
+		//Red
 		led.setR(true);
 		delay(400);
 		led.setR(false);
 		delay(200);
 
+		//Red
 		led.setR(true);
 		delay(400);
 		led.setR(false);
 		delay(200);
 
+		//Red
 		led.setR(true);
 		delay(400);
 		led.setR(false);
 		delay(200);
 
+		//Green!
 		led.setG(true);
 		delay(800);
 		led.setG(false);
@@ -36,20 +58,35 @@ namespace EuclidRobot
 
 	void Robot::followLine()
 	{
+		//Set the mode of the motors.
 		leftMotor.setMode(BRAKE);
-		rightMotor.setMode(COAST);
+		rightMotor.setMode(BRAKE);
 
+		//Initialize the PID Controller.
 		PIDController controller(200, 0, 10);
 		controller.start(0);
 
 		//Infinite loop.
 		while (true)
 		{
+			/*
+			Read the center of the line.
+			This value affects the hue of the RGB LED:
+			Far Left: Red
+			Center: Green
+			Far Right: Blue
+			*/
 			float error = sensorArray.readLineCenter();
 			led.setHSV((error + 1) / 3.0f, 1, 1);
 
+			/*
+			Get the correction from the controller.
+			*/
 			float correction = controller.getCorrection(error);
 
+			/*
+			Set the turning of the bot based on this correction.
+			*/
 			moveForward(1, correction);
 			delay(10);
 		}
@@ -59,11 +96,13 @@ namespace EuclidRobot
 	{
 		if (turning >= 0)
 		{
+			//Turn to the right.
 			leftMotor.setSpeed(speed);
 			rightMotor.setSpeed(speed * (1 - turning));
 		}
 		else
 		{
+			//Turn to the left.
 			leftMotor.setSpeed(speed * (1 + turning));
 			rightMotor.setSpeed(speed);
 		}
--- Robot.h
+++ Robot.h
@@ -1,3 +1,20 @@
+/*
+--FILE--
+Robot.h
+
+--AUTHOR--
+Name:		Josh Alan
+GitHub:		theDataSmith
+E-mail:		thedatasmith1@gmail.com
+
+--PROJECT--
+Euclid, the line-following robot.
+GitHub:		github.com/theDataSmith/euclid
+
+--CREATION DATE--
+11 / 26 / 2016
+*/
+
 #pragma once
 
 #include "LED.h"
@@ -7,6 +24,11 @@
 
 namespace EuclidRobot
 {
+	/*
+	The robot interfaces with each component through one or more pins.
+	These pins can be used to send or recieve data, or even power low-power devices, such as LEDs.
+	Each pin has a number, and those numbers are defined here.
+	*/
 	static const int PIN_LED = 13;
 	static const int PIN_LED_R = 6;
 	static const int PIN_LED_G = 5;
@@ -20,10 +42,20 @@ namespace EuclidRobot
 	static const int PIN_MOTOR_RIGHT_FWD = 20;
 	static const int PIN_MOTOR_RIGHT_BWD = 21;
 
+	/*
+	This robot will follow a black line on a white surface.
+	It has two wheels in the back, each controlled by a motor.
+	It uses 6 sensors in the front of the robot to sense the location of the black line.
+	It also has an RGB LED for output.
+	*/
 	class Robot
 	{
 	public:
 
+		/*
+		Initializes hardware objects.
+		Turns on the LED on the Teensy board.
+		*/
 		Robot();
 
 		/*
@@ -37,13 +69,35 @@ namespace EuclidRobot
 		void followLine();
 
 	private:
+		/*
+		This object handles the LED on the Teensy board,
+		as well as the RGB LED.
+		*/
 		LED led;
 
+		/*
+		Interfaces with the array of 6 reflectance sensors
+		that read the location of the black line.
+		*/
 		SensorArray sensorArray;
 
+		/*
+		These objects interface with the H-bridge motor driver,
+		which directly controls the two motors.
+		*/
 		Motor leftMotor;
 		Motor rightMotor;
 
+		/*
+		Sets the speed of the two motors.
+		speed controls how fast it goes.
+		turning lowers the speed of one of the motors to turn it:
+		-2: Rotate left in place.
+		-1: Turn left.
+		0: Straight ahead.
+		1: Turn right.
+		2: Rotate right in place.
+		*/
 		void moveForward(float speed, float turning);
 	};
 }
